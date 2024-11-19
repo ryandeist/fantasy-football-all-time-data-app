7f9ed2101ba18b3580bdf4e3e013b0fdd4784dbb
ryandeist <ryandeist@gmail.com>
2024-11-19 14:42:28 -0500

    added coach model

---
+++ models/coach.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+//create coach schema
+const coachSchema = new mongoose.Schema({
+    name: String,
+    regularSeasonWins: Number,
+    regularSeasonLosses: Number,
+    regularSeasonWinningPercentage: Number,
+    playoffBerths: Number,
+    playoffByes: Number, 
+    playoffGames: Number,
+    playoffWins: Number,
+    playoffLosses: Number,
+    playoffWinPercentage: Number,
+    superBowlAppearances: Number,
+    superBowlWins: Number,
+    topThreeFinishes: Number,
+});
+
+// create coach model
+const Coach = mongoose.model("Coach", coachSchema);
+
+// export coach model
+module.exports = Coach;
