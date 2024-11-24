1181a6b875dac5c552180336a1f7342a2cc9d527
ryandeist <ryandeist@gmail.com>
2024-11-23 19:35:06 -0500

    cards styled, isActive styling added

--- middleware/adminCheck.js
+++ middleware/adminCheck.js
@@ -1,5 +1,5 @@
 const adminCheck = (req, res, next) => {
-    if (req.session.user.isAdmin) return next()
+    if (req.session.user && req.session.user.isAdmin) return next()
         res.redirect('/');
 };
 
